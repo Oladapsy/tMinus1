ba461776e8431af4d5ef4f15ad9d5c0aa8c31d4a
oladapsy <dapoodedeyi@yahoo.com>
2026-04-29 09:10:33 +0100

    loading font with use font

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,29 @@
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
 import { Stack } from "expo-router";
+import { useEffect } from "react";
 
 export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    "NeueMontreal-Regular": require("@/assets/fonts/NeueMontreal-Regular.otf"),
+    "NeueMontreal-Medium": require("@/assets/fonts/NeueMontreal-Medium.otf"),
+    "NeueMontreal-Bold": require("@/assets/fonts/NeueMontreal-Bold.otf"),
+    "NeueMontreal-Light": require("@/assets/fonts/NeueMontreal-Light.otf"),
+    "NeueMontreal-Italic": require("@/assets/fonts/NeueMontreal-Italic.otf"),
+    "NeueMontreal-MediumItalic": require("@/assets/fonts/NeueMontreal-MediumItalic.otf"),
+    "NeueMontreal-BoldItalic": require("@/assets/fonts/NeueMontreal-BoldItalic.otf"),
+    "NeueMontreal-LightItalic": require("@/assets/fonts/NeueMontreal-LightItalic.otf"),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return <Stack />;
 }
