b3202aa55413fc8068ffd2f23d8e991fd347150e
oladapsy <dapoodedeyi@yahoo.com>
2026-04-29 09:12:54 +0100

    font family object setup

---
+++ src/constants/fontFamily.ts
@@ -0,0 +1,10 @@
+export const FontFamily = {
+  regular: "NeueMontreal-Regular",
+  medium: "NeueMontreal-Medium",
+  bold: "NeueMontreal-Bold",
+  light: "NeueMontreal-Light",
+  italic: "NeueMontreal-Italic",
+  mediumItalic: "NeueMontreal-MediumItalic",
+  boldItalic: "NeueMontreal-BoldItalic",
+  lightItalic: "NeueMontreal-LightItalic",
+};
