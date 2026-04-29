483c9256d8cb44dadcfc3cd2ee4ad48a8b866f4f
oladapsy <dapoodedeyi@yahoo.com>
2026-04-29 12:35:42 +0100

    onboarding screen

--- app/onboarding/index.tsx
+++ app/onboarding/index.tsx
@@ -1,10 +1,14 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
+import MySafeAreaView from "@/src/components/common/MySafeAreaView";
+import { Colors } from "@/src/constants/colors";
 
 export default function Onboarding1() {
   return (
-    <View>
-      <Text>Onboarding 1</Text>
-    </View>
-  )
+    <MySafeAreaView color={Colors.primary}>
+      <View>
+        <Text>Onboarding 1</Text>
+      </View>
+    </MySafeAreaView>
+  );
 }
