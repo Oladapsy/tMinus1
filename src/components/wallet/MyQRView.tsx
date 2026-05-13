2fcbbd507b0b9ef61b03de010c79c2c76453312c
oladapsy <dapoodedeyi@yahoo.com>
2026-05-13 15:50:21 +0100

    feature: qr code implementation

--- src/components/wallet/MyQRView.tsx
+++ src/components/wallet/MyQRView.tsx
@@ -1,17 +1,137 @@
-import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, TouchableOpacity, View } from "react-native";
 import { QrCodeSvg, plainRenderer } from "react-native-qr-svg";
+import Paragraph from "../common/Paragraph";
+import Title from "../common/Title";
+import PrimaryButton from "../common/PrimaryButton";
+import CameraIcon from "@/assets/icons/qr/qr-code1.svg";
+import CopyIcon from "@/assets/icons/qr/copy2.svg";
+import { Colors } from "@/src/constants/colors";
+import * as Clipboard from "expo-clipboard";
 
 interface Props {
-  address: string;
+  btcAddress: string;
+  usdAddress: string;
+  usdBalance: string;
+  btcBalance: string;
   onScanQR: () => void;
 }
-export const MyQRView = () => {
+export const MyQRView = ({
+  btcAddress = "btc5dirgMNYdQskfiP5zj39VYemXareK4C",
+  usdAddress = "usdt5dirgMNYdQskfiP5zj39VYemXareK4C",
+  usdBalance,
+  btcBalance,
+  onScanQR,
+}: Props) => {
+  const [unit, setUnit] = useState<"USD" | "BTC">("USD");
+  const copyToClipboard = async () => {
+    const address = unit === "USD" ? usdAddress : btcAddress;
+    await Clipboard.setStringAsync(address);
+  };
+
+  const currentAddress = unit === "USD" ? usdAddress : btcAddress;
+  const currentBalance = unit === "USD" ? usdBalance : btcBalance;
+
   return (
-    <View>
-      <Text>QRCOde</Text>
+    <View style={style.container}>
+      {/* Head */}
+
+      <View style={style.toggleRow}>
+        {/* onclick should switch the qr-code and adress and on select have another color*/}
+        <View>
+          <TouchableOpacity onPress={() => setUnit("USD")}>
+            <Paragraph
+              text="USD"
+              color={unit === "USD" ? Colors.green : Colors.secondary}
+              size={14}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => setUnit("BTC")}>
+            <Paragraph
+              text="BTC"
+              color={unit === "BTC" ? Colors.green : Colors.secondary}
+              size={14}
+            />
+          </TouchableOpacity>
+        </View>
+
+        {/* Balance */}
+        <Title text={`${unit} ${currentBalance}`} size={28} />
+      </View>
+
+      {/* QR CODE */}
+      <View style={style.qrWrapper}>
+        <QrCodeSvg
+          style={style.qr}
+          renderer={plainRenderer}
+          value={currentAddress}
+          frameSize={100}
+        />
+      </View>
+
+      {/* Footer */}
+      <Paragraph text="address" size={14} />
+
+      {/* Adress and a copy at the end */}
+      <View style={style.addressRow}>
+        <Paragraph text={currentAddress} size={14} textAlign="left" />
+
+        <TouchableOpacity onPress={copyToClipboard}>
+          <CopyIcon />
+        </TouchableOpacity>
+      </View>
+
+      {/* the copy Address with copy icon */}
+      <Paragraph
+        text="Carefull to use the right address we will not be responsible for any loss"
+        size={14}
+      />
+      <PrimaryButton
+        text="Scan QR code"
+        icon={<CameraIcon />}
+        onPress={onScanQR}
+      />
     </View>
   );
 };
 
-const style = StyleSheet.create({});
+const style = StyleSheet.create({
+  container: {},
+  toggleRow: {},
+  qrWrapper: {},
+  addressRow: {},
+  tab: {},
+  qr: {
+    padding: 15,
+  },
+  adressCopy: {
+    backgroundColor: Colors.tertiary,
+  },
+  //   container: {
+  //     paddingHorizontal: 24,
+  //     paddingTop: 20,
+  //     gap: 20,
+  //   },
+  //   toggleRow: {
+  //     flexDirection: "row",
+  //     gap: 20,
+  //   },
+  //   qrWrapper: {
+  //     alignItems: "center",
+  //     justifyContent: "center",
+  //     marginVertical: 20,
+  //   },
+  //   qr: {
+  //     padding: 15,
+  //     backgroundColor: "white",
+  //     borderRadius: 12,
+  //   },
+  //   addressRow: {
+  //     backgroundColor: Colors.tertiary,
+  //     padding: 12,
+  //     borderRadius: 10,
+  //     flexDirection: "row",
+  //     justifyContent: "space-between",
+  //     alignItems: "center",
+  //   },
+});
--- src/components/wallet/ScanView.tsx
+++ src/components/wallet/ScanView.tsx
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, StyleSheet, TouchableOpacity } from "react-native";
 import React from "react";
 import Title from "@/src/components/common/Title";
 import CameraIcon from "@/assets/icons/qr/smallCamera.svg";
@@ -8,36 +8,58 @@ import { Colors } from "@/src/constants/colors";
 import ScanIcon from "@/assets/icons/qr/qr-code1.svg";
 
 interface Props {
-  permission: string;
-  scanned: string;
-  onScanned: string;
-  onShowQR: string;
-  onRequestPermission: string;
+  permission: any;
+  scanned: boolean;
+  onScanned: (data: any) => void;
+  onShowQR: () => void;
+  onRequestPermission: () => void;
 }
-export default function ScanView() {
+
+export default function ScanView({
+  permission,
+  scanned,
+  onScanned,
+  onShowQR,
+  onRequestPermission,
+}: Props) {
   return (
-    <View style={Styles.container}>
-      {/* head */}
-      <View style={Styles.cameraAndText}>
+    <View style={styles.container}>
+      {/* Header */}
+      <View style={styles.cameraAndText}>
         <CameraIcon />
         <Title text="Scan QR code" size={18} />
       </View>
 
-      <View style={Styles.headParagraph}>
+      <View style={styles.headParagraph}>
         <Paragraph
-          text="Scan the QR code and it automatically recognize it."
+          text="Scan the QR code and it will be recognized automatically."
           size={14}
           textAlign="center"
           lineHeight={20}
         />
       </View>
 
-      {/* Body */}
+      {/* Placeholder for camera */}
+      <TouchableOpacity
+        style={styles.cameraPlaceholder}
+        onPress={onRequestPermission}
+      >
+        <Paragraph
+          text="Tap to start scanning"
+          size={14}
+          color={Colors.mediumGray}
+        />
+      </TouchableOpacity>
 
-      {/* Footer */}
-      <View style={Styles.button1}>
-        <PrimaryButton text="Show QR code" icon={<ScanIcon />} />
+      {/* Buttons */}
+      <View style={styles.button1}>
+        <PrimaryButton
+          text="Show QR code"
+          icon={<ScanIcon />}
+          onPress={onShowQR}
+        />
       </View>
+
       <PrimaryButton
         text="Cancel"
         Bgcolor={Colors.secondary}
@@ -47,21 +69,31 @@ export default function ScanView() {
   );
 }
 
-const Styles = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingHorizontal: 24,
+    paddingTop: 24,
   },
   cameraAndText: {
     flexDirection: "row",
     gap: 10,
     justifyContent: "center",
     alignItems: "center",
-    marginTop: 24,
     marginBottom: 4,
   },
   headParagraph: {
-    paddingHorizontal: 60,
-    marginBottom: 16,
+    paddingHorizontal: 40,
+    marginBottom: 20,
+  },
+  cameraPlaceholder: {
+    height: 260,
+    borderRadius: 16,
+    borderWidth: 1,
+    borderColor: Colors.thinWhite,
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 40,
   },
   button1: {
     marginBottom: 20,
