d18b9a1cfda1385025d26b8ce2ebd73732e8a324
oladapsy <dapoodedeyi@yahoo.com>
2026-05-28 08:58:35 +0100

    feature: wallet screen

--- app/(tabs)/wallets/MainWalletScreen.tsx
+++ app/(tabs)/wallets/MainWalletScreen.tsx
@@ -10,6 +10,7 @@ import { Colors } from "@/src/constants/colors";
 import { FontFamily } from "@/src/constants/fonts";
 import React, { useState } from "react";
 import { StyleSheet, ImageBackground, View } from "react-native";
+
 import bgImage from "@/assets/images/wallet/WalletBg.png";
 
 const MainWalletScreen = () => {
@@ -20,84 +21,107 @@ const MainWalletScreen = () => {
 
   const Balance = "40,059.83";
   const UsdValue = "$468,554.23";
+
   return (
-    <MySafeAreaView style={styles.container}>
-        <View style={styles.balance}>
-          <Paragraph text="Current Balance" textAlign="left" size={14} />
-          <View style={styles.eyeIcon}>
-            <IconAndText
-              onPress={() => setShowBalance(!showBalance)}
-              icon={
-                showBalance ? (
-                  <EyeClosed color={Colors.secondary} />
-                ) : (
-                  <EyeOpen />
-                )
+    <MySafeAreaView style={styles.container} edges={['bottom', 'left', 'right']} >
+      {/* Background Section */}
+      <ImageBackground
+        source={bgImage}
+        resizeMode="cover"
+        style={styles.headerBg}
+      >
+        <View style={styles.content}>
+          <View style={styles.balance}>
+            <Paragraph text="Current Balance" textAlign="left" size={14} />
+
+            <View style={styles.eyeIcon}>
+              <IconAndText
+                onPress={() => setShowBalance(!showBalance)}
+                icon={
+                  showBalance ? (
+                    <EyeClosed color={Colors.secondary} />
+                  ) : (
+                    <EyeOpen />
+                  )
+                }
+              />
+            </View>
+          </View>
+
+          <View>
+            <View style={styles.mainBalance}>
+              <Title
+                text={showBalance ? Balance : "********"}
+                size={32}
+                fontFamily={FontFamily.bold}
+              />
+            </View>
+
+            <Paragraph
+              text={showBalance ? UsdValue : "********"}
+              textAlign="left"
+            />
+          </View>
+
+          {/* Tabs */}
+          <View style={styles.tab}>
+            <PrimaryButton
+              text="Deposit"
+              fullWidth={false}
+              style={{ flex: 1, height: 46 }}
+              fontSize={16}
+              Bgcolor={mode === "deposit" ? Colors.green : Colors.lightPrimary}
+              textColor={mode === "deposit" ? Colors.primary : Colors.secondary}
+              onPress={() => setMode("deposit")}
+            />
+
+            <PrimaryButton
+              text="Withdraw"
+              fullWidth={false}
+              style={{ flex: 1, height: 46 }}
+              fontSize={16}
+              Bgcolor={
+                mode === "withdrawl" ? Colors.green : Colors.lightPrimary
               }
+              textColor={
+                mode === "withdrawl" ? Colors.primary : Colors.secondary
+              }
+              onPress={() => setMode("withdrawl")}
+            />
+
+            <PrimaryButton
+              text="Transfer"
+              fullWidth={false}
+              style={{ flex: 1, height: 46 }}
+              fontSize={16}
+              Bgcolor={mode === "transfer" ? Colors.green : Colors.lightPrimary}
+              textColor={
+                mode === "transfer" ? Colors.primary : Colors.secondary
+              }
+              onPress={() => setMode("transfer")}
             />
           </View>
         </View>
+      </ImageBackground>
 
-        <View>
-          <View style={styles.mainBalance}>
-            <Title
-              text={showBalance ? Balance : "********"}
-              size={32}
-              fontFamily={FontFamily.bold}
-            />
-          </View>
+      {/* Content */}
+      <View style={styles.body}>
+        {mode === "deposit" && <Deposit />}
 
+        {mode === "withdrawl" && (
           <Paragraph
-            text={showBalance ? UsdValue : "********"}
+            text="Withdraw screen content goes here..."
             textAlign="left"
           />
-        </View>
+        )}
 
-        {/* Tabs|| Deposit || Withdraw || Transfer */}
-        <View style={styles.tab}>
-          <PrimaryButton
-            text="Deposit"
-            fullWidth={false}
-            style={{ flex: 1, height: 46 }}
-            fontSize={16}
-            Bgcolor={mode === "deposit" ? Colors.green : Colors.lightPrimary} // 👈 active styling
-            textColor={mode === "deposit" ? Colors.primary : Colors.secondary}
-            onPress={() => setMode("deposit")}
+        {mode === "transfer" && (
+          <Paragraph
+            text="Transfer screen content goes here..."
+            textAlign="left"
           />
-          <PrimaryButton
-            text="Withdraw"
-            fullWidth={false}
-            style={{ flex: 1, height: 46 }}
-            fontSize={16}
-            Bgcolor={mode === "withdrawl" ? Colors.green : Colors.lightPrimary} // 👈 active styling
-            textColor={mode === "withdrawl" ? Colors.primary : Colors.secondary}
-            onPress={() => setMode("withdrawl")}
-          />
-          <PrimaryButton
-            text="Transfer"
-            fullWidth={false}
-            style={{ flex: 1, height: 46 }}
-            Bgcolor={mode === "transfer" ? Colors.green : Colors.lightPrimary} // 👈 active styling
-            textColor={mode === "transfer" ? Colors.primary : Colors.secondary}
-            fontSize={16}
-            onPress={() => setMode("transfer")}
-          />
-        </View>
-
-      {/* Render tabs */}
-      {mode === "deposit" && <Deposit />}
-      {mode === "withdrawl" && (
-        <Paragraph
-          text="Withdraw screen content goes here..."
-          textAlign="left"
-        />
-      )}
-      {mode === "transfer" && (
-        <Paragraph
-          text="Transfer screen content goes here..."
-          textAlign="left"
-        />
-      )}
+        )}
+      </View>
     </MySafeAreaView>
   );
 };
@@ -106,25 +130,39 @@ export default MainWalletScreen;
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     backgroundColor: Colors.primary,
+  },
+  headerBg: {
+    paddingTop: 70,
+    paddingBottom: 32,
+  },
+  content: {
+    paddingHorizontal: 24,
+  },
+
+  body: {
     flex: 1,
     paddingHorizontal: 24,
   },
+
   balance: {
     flexDirection: "row",
     justifyContent: "space-between",
   },
+
   tab: {
     flexDirection: "row",
     justifyContent: "space-between",
     gap: 8,
     marginTop: 54,
   },
+
   mainBalance: {
     marginTop: 2,
   },
+
   eyeIcon: {
     marginTop: 10,
   },
-  image: {},
 });
