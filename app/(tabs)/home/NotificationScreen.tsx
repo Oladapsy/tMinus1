1be83c6134f642cf2fd0b1cd847d3e242f571c82
oladapsy <dapoodedeyi@yahoo.com>
2026-07-11 12:14:30 +0100

    err console

--- app/(tabs)/home/NotificationScreen.tsx
+++ app/(tabs)/home/NotificationScreen.tsx
@@ -57,6 +57,7 @@ export default function NotificationScreen() {
       showToast("All notifications marked as read!", "success");
     } catch (err) {
       showToast("Failed to clear notifications.", "error");
+      console.log(err)
     }
   };
 
